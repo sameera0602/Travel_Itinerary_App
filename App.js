43880d43598173dc757faa4efe11564cbc9b8096
sameera0602 <sasaum06@gmail.com>
2025-04-27 11:08:25 +0300

    App.js

--- App.js
+++ App.js
@@ -3,10 +3,16 @@ import './App.css';
 
 import Onboarding from './components/Onboarding';
 import Dashboard from './components/Dashboard';
+import ThirdPane from './components/ThirdPane'; 
 
 function App() {
   const [screen, setScreen] = useState('onboarding');
   const [darkMode, setDarkMode] = useState(false);
+  const [isThirdPaneOpen, setIsThirdPaneOpen] = useState(false);
+
+  const toggleThirdPane = () => {
+    setIsThirdPaneOpen(!isThirdPaneOpen);
+  };
 
   return (
     <>
@@ -23,8 +29,14 @@ function App() {
         />
       )}
 
+      {/* Button to toggle third pane */}
+      <button onClick={toggleThirdPane}>Click to Open Third Pane</button>
+
+      {/* Show third pane if it's open */}
+      {isThirdPaneOpen && <ThirdPane />}
     </>
   );
 }
 
 export default App;
+
